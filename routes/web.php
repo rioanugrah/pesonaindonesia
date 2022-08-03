b9b887c12664e96e5bbb629ce7cbc8cb7cf88330
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2022-08-03 19:56:13 +0700

    Plesiran Indonesia

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::group(['domain' => 'api.localhost'], function () {
 });
 
 Route::group(['domain' => 'app.localhost'], function () {
-    Route::get('/', 'Apps\HomeController@index');
+    // Route::get('/', 'Apps\HomeController@index');
     // {
         // return view('auth.login');
         // return "This will respond to requests for 'admin.localhost/'";
@@ -75,7 +75,7 @@ Route::group(['domain' => 'partner.localhost'], function () {
     Route::get('/', 'FrontendController@partnership')->name('frontend.partnership');
 });
 
-Route::get('/', 'FrontendController@index')->name('frontend');
+// Route::get('/', 'FrontendController@index')->name('frontend');
 // Route::get('login', function(){
 //     return view('auth.login');
 // });
@@ -153,119 +153,127 @@ Route::get('{driver}/callback', 'Auth\LoginController@handleProviderCallback')->
 // Route::get('auth/google', 'Auth\LoginController@redirectToProvider')->name('login_google');
 // Route::get('auth/google/callback', 'Auth\LoginController@handleProviderCallback');
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::prefix('b')->group(function () {
-        Route::get('testing', function(){
-            return view('layouts.backend_5.app');
-        });
-        Route::get('home', 'HomeController@index')->name('home')->middleware('verified');
-        Route::get('home/balance', 'HomeController@balance')->name('home.balance')->middleware('verified');
-        Route::get('wisatas', 'WisataController@index')->name('wisata')->middleware('verified');
+// Route::group(['middleware' => 'auth'], function () {
+//     Route::prefix('b')->group(function () {
+//         Route::get('testing', function(){
+//             return view('layouts.backend_5.app');
+//         });
+//         Route::get('home', 'HomeController@index')->name('home')->middleware('verified');
+//         Route::get('home/balance', 'HomeController@balance')->name('home.balance')->middleware('verified');
+//         Route::get('wisatas', 'WisataController@index')->name('wisata')->middleware('verified');
         
-        Route::get('kategori_kota', 'KategoriKotaController@index')->name('ktkota')->middleware('verified');
-        Route::post('kategori_kota/simpan', 'KategoriKotaController@simpan')->name('ktkota.simpan')->middleware('verified');
+//         Route::get('kategori_kota', 'KategoriKotaController@index')->name('ktkota')->middleware('verified');
+//         Route::post('kategori_kota/simpan', 'KategoriKotaController@simpan')->name('ktkota.simpan')->middleware('verified');
         
-        Route::prefix('cooperation')->group(function() {
-            Route::get('/', 'CooperationController@index')->name('cooperation')->middleware('verified');
-            Route::get('notif', 'CooperationController@notif')->middleware('verified');
-            Route::get('{id}/download', 'CooperationController@download')->name('cooperation.download')->middleware('verified');
-            Route::get('{id}', 'CooperationController@detail')->middleware('verified');
-            Route::post('upload', 'CooperationController@upload_berkas')->name('cooperation.upload_berkas')->middleware('verified');
-            Route::post('data/upload', 'CooperationController@berkas')->name('cooperation.berkas')->middleware('verified');
-            Route::post('status', 'CooperationController@status')->name('cooperation.status')->middleware('verified');
-        });
+//         Route::prefix('cooperation')->group(function() {
+//             Route::get('/', 'CooperationController@index')->name('cooperation')->middleware('verified');
+//             Route::get('notif', 'CooperationController@notif')->middleware('verified');
+//             Route::get('{id}/download', 'CooperationController@download')->name('cooperation.download')->middleware('verified');
+//             Route::get('{id}', 'CooperationController@detail')->middleware('verified');
+//             Route::post('upload', 'CooperationController@upload_berkas')->name('cooperation.upload_berkas')->middleware('verified');
+//             Route::post('data/upload', 'CooperationController@berkas')->name('cooperation.berkas')->middleware('verified');
+//             Route::post('status', 'CooperationController@status')->name('cooperation.status')->middleware('verified');
+//         });
         
-        Route::get('pengguna', 'UsersController@index')->name('pengguna')->middleware('verified');
+//         Route::get('pengguna', 'UsersController@index')->name('pengguna')->middleware('verified');
         
-        Route::get('roles', 'RolesController@index')->name('roles')->middleware('verified');
-        Route::post('roles/simpan', 'RolesController@simpan')->name('roles.simpan')->middleware('verified');
-        Route::get('roles/{id}/edit', 'RolesController@edit')->name('roles.edit')->middleware('verified');
-        Route::get('roles/{slug}', 'RolesController@detail')->name('roles.detail')->middleware('verified');
-        Route::post('roles/update', 'RolesController@update')->name('roles.update')->middleware('verified');
+//         Route::get('roles', 'RolesController@index')->name('roles')->middleware('verified');
+//         Route::post('roles/simpan', 'RolesController@simpan')->name('roles.simpan')->middleware('verified');
+//         Route::get('roles/{id}/edit', 'RolesController@edit')->name('roles.edit')->middleware('verified');
+//         Route::get('roles/{slug}', 'RolesController@detail')->name('roles.detail')->middleware('verified');
+//         Route::post('roles/update', 'RolesController@update')->name('roles.update')->middleware('verified');
        
-        Route::get('status', 'StatusController@index')->name('status')->middleware('verified');
+//         Route::get('status', 'StatusController@index')->name('status')->middleware('verified');
         
-        Route::get('slider', 'SliderController@index')->name('slider')->middleware('verified');
-        Route::get('slider/{id}/edit', 'SliderController@edit')->name('slider.edit')->middleware('verified');
-        Route::post('slider/update', 'SliderController@update')->name('slider.update')->middleware('verified');
-        Route::get('slider/{id}/hapus', 'SliderController@delete')->name('slider.hapus')->middleware('verified');
+//         Route::get('slider', 'SliderController@index')->name('slider')->middleware('verified');
+//         Route::get('slider/{id}/edit', 'SliderController@edit')->name('slider.edit')->middleware('verified');
+//         Route::post('slider/update', 'SliderController@update')->name('slider.update')->middleware('verified');
+//         Route::get('slider/{id}/hapus', 'SliderController@delete')->name('slider.hapus')->middleware('verified');
         
-        Route::get('tiket_wisata', 'TiketWisataController@index_tiket_wisata')->name('tiket_wisata')->middleware('verified');
+//         Route::get('tiket_wisata', 'TiketWisataController@index_tiket_wisata')->name('tiket_wisata')->middleware('verified');
        
-        Route::get('hotel', 'HotelController@index')->name('hotel')->middleware('verified');
-        Route::post('hotel/simpan', 'HotelController@simpan')->name('hotel.simpan')->middleware('verified');
-        Route::post('hotel/upload_image', 'HotelController@upload_image')->name('hotel.upload_image')->middleware('verified');
-        Route::post('hotel/update', 'HotelController@update')->name('hotel.update')->middleware('verified');
-        Route::get('hotel/{id}/image', 'HotelController@detail_image')->middleware('verified');
-        Route::get('hotel/{id}', 'HotelController@detail')->name('hotel.detail')->middleware('verified');
-        Route::get('hotel/{id}/edit', 'HotelController@edit')->name('hotel.edit')->middleware('verified');
-        Route::get('hotel/{id}/kamar', 'KamarHotelController@index')->name('kamar')->middleware('verified');
-        Route::post('hotel/kamar/simpan', 'KamarHotelController@simpan')->name('kamar.simpan')->middleware('verified');
-        Route::get('hotel/delete/{id}', 'HotelController@destroy')->name('hotel.hapus')->middleware('verified');
-        Route::post('hotel/export', 'HotelController@export')->name('hotel.export')->middleware('verified');
-        Route::post('hotel/import', 'HotelController@import')->name('hotel.import')->middleware('verified');
-        Route::get('hotel/checkroom/{id}', 'HotelController@checkRoom')->name('hotel.checkroom')->middleware('verified');
-        // Route::get('hotel/importExportView', 'DemoController@importExportView');
+//         Route::get('hotel', 'HotelController@index')->name('hotel')->middleware('verified');
+//         Route::post('hotel/simpan', 'HotelController@simpan')->name('hotel.simpan')->middleware('verified');
+//         Route::post('hotel/upload_image', 'HotelController@upload_image')->name('hotel.upload_image')->middleware('verified');
+//         Route::post('hotel/update', 'HotelController@update')->name('hotel.update')->middleware('verified');
+//         Route::get('hotel/{id}/image', 'HotelController@detail_image')->middleware('verified');
+//         Route::get('hotel/{id}', 'HotelController@detail')->name('hotel.detail')->middleware('verified');
+//         Route::get('hotel/{id}/edit', 'HotelController@edit')->name('hotel.edit')->middleware('verified');
+//         Route::get('hotel/{id}/kamar', 'KamarHotelController@index')->name('kamar')->middleware('verified');
+//         Route::post('hotel/kamar/simpan', 'KamarHotelController@simpan')->name('kamar.simpan')->middleware('verified');
+//         Route::get('hotel/delete/{id}', 'HotelController@destroy')->name('hotel.hapus')->middleware('verified');
+//         Route::post('hotel/export', 'HotelController@export')->name('hotel.export')->middleware('verified');
+//         Route::post('hotel/import', 'HotelController@import')->name('hotel.import')->middleware('verified');
+//         Route::get('hotel/checkroom/{id}', 'HotelController@checkRoom')->name('hotel.checkroom')->middleware('verified');
+//         // Route::get('hotel/importExportView', 'DemoController@importExportView');
         
-        Route::get('perusahaan', 'PerusahaanController@index')->name('perusahaan')->middleware('verified');
-        Route::post('perusahaan/simpan', 'PerusahaanController@simpan')->name('perusahaan.simpan')->middleware('verified');
-        Route::get('perusahaan/{id}/edit', 'PerusahaanController@edit')->name('perusahaan.edit')->middleware('verified');
-        Route::post('perusahaan/update', 'PerusahaanController@update')->name('perusahaan.update')->middleware('verified');
-        Route::get('perusahaan/delete/{id}', 'PerusahaanController@destroy')->name('perusahaan.hapus')->middleware('verified');
+//         Route::get('perusahaan', 'PerusahaanController@index')->name('perusahaan')->middleware('verified');
+//         Route::post('perusahaan/simpan', 'PerusahaanController@simpan')->name('perusahaan.simpan')->middleware('verified');
+//         Route::get('perusahaan/{id}/edit', 'PerusahaanController@edit')->name('perusahaan.edit')->middleware('verified');
+//         Route::post('perusahaan/update', 'PerusahaanController@update')->name('perusahaan.update')->middleware('verified');
+//         Route::get('perusahaan/delete/{id}', 'PerusahaanController@destroy')->name('perusahaan.hapus')->middleware('verified');
 
-        Route::get('log', 'LogController@index')->name('log')->middleware('verified');
+//         Route::get('log', 'LogController@index')->name('log')->middleware('verified');
         
-        Route::get('events', 'EventsController@index')->name('events')->middleware('verified');
-        Route::post('events/simpan', 'EventsController@simpan')->name('events.simpan')->middleware('verified');
-        Route::get('events/{id}', 'EventsController@detail')->name('events.detail')->middleware('verified');
-        Route::get('events/{id}/register', 'EventsController@detailEventRegister')->name('events.detailRegister')->middleware('verified');
-        Route::get('events/delete/{id}', 'EventsController@destroy')->name('events.delete')->middleware('verified');
+//         Route::get('events', 'EventsController@index')->name('events')->middleware('verified');
+//         Route::post('events/simpan', 'EventsController@simpan')->name('events.simpan')->middleware('verified');
+//         Route::get('events/{id}', 'EventsController@detail')->name('events.detail')->middleware('verified');
+//         Route::get('events/{id}/register', 'EventsController@detailEventRegister')->name('events.detailRegister')->middleware('verified');
+//         Route::get('events/delete/{id}', 'EventsController@destroy')->name('events.delete')->middleware('verified');
 
-        Route::get('post', 'PostController@index')->name('post')->middleware('verified');
-        Route::post('post/simpan', 'PostController@simpan')->name('post.simpan')->middleware('verified');
-    });
+//         Route::get('post', 'PostController@index')->name('post')->middleware('verified');
+//         Route::post('post/simpan', 'PostController@simpan')->name('post.simpan')->middleware('verified');
+//     });
 
-    // Route::get('hotel/export', function(){
-    //     return 'Export Hotel';
-    // });
+//     // Route::get('hotel/export', function(){
+//     //     return 'Export Hotel';
+//     // });
 
-    Route::get('country/simpan', 'CountryController@simpan');
+//     Route::get('country/simpan', 'CountryController@simpan');
 
-    Route::post('wisata/simpan', 'WisataController@simpan')->name('wisata.simpan')->middleware('verified');
+//     Route::post('wisata/simpan', 'WisataController@simpan')->name('wisata.simpan')->middleware('verified');
     
-    Route::post('cooperation/simpan', 'CooperationController@simpan')->name('cooperation.simpan')->middleware('verified');
-    Route::post('cooperation/kab_kota', 'CooperationController@select_kab_kota')->middleware('verified');
+//     Route::post('cooperation/simpan', 'CooperationController@simpan')->name('cooperation.simpan')->middleware('verified');
+//     Route::post('cooperation/kab_kota', 'CooperationController@select_kab_kota')->middleware('verified');
     
-    Route::post('hotel/kab_kota', 'HotelController@select_kab_kota')->middleware('verified');
-    Route::post('hotel/kecamatan', 'HotelController@select_kecamatan')->middleware('verified');
+//     Route::post('hotel/kab_kota', 'HotelController@select_kab_kota')->middleware('verified');
+//     Route::post('hotel/kecamatan', 'HotelController@select_kecamatan')->middleware('verified');
     
-    Route::get('pengguna/{id}', 'UsersController@detail')->middleware('verified');
-    Route::get('pengguna/delete/{id}', 'UsersController@delete')->middleware('verified');
-    Route::post('pengguna/simpan', 'UsersController@simpan')->name('pengguna.simpan')->middleware('verified');
-    Route::post('pengguna/update', 'UsersController@update')->name('pengguna.update')->middleware('verified');
+//     Route::get('pengguna/{id}', 'UsersController@detail')->middleware('verified');
+//     Route::get('pengguna/delete/{id}', 'UsersController@delete')->middleware('verified');
+//     Route::post('pengguna/simpan', 'UsersController@simpan')->name('pengguna.simpan')->middleware('verified');
+//     Route::post('pengguna/update', 'UsersController@update')->name('pengguna.update')->middleware('verified');
     
-    Route::post('status/simpan', 'StatusController@simpan')->name('status.simpan')->middleware('verified');
+//     Route::post('status/simpan', 'StatusController@simpan')->name('status.simpan')->middleware('verified');
     
-    Route::post('slider/simpan', 'SliderController@simpan')->name('slider.simpan')->middleware('verified');
+//     Route::post('slider/simpan', 'SliderController@simpan')->name('slider.simpan')->middleware('verified');
     
-    Route::get('tiket_wisata/{id}/{created_at}', 'TiketWisataController@cekTiket')->name('detail_tiket_wisata')->middleware('verified');
+//     Route::get('tiket_wisata/{id}/{created_at}', 'TiketWisataController@cekTiket')->name('detail_tiket_wisata')->middleware('verified');
     
-    Route::get('testing', 'ChatController@chat')->name('chat')->middleware('verified');
+//     Route::get('testing', 'ChatController@chat')->name('chat')->middleware('verified');
     
-    Route::get('payment', 'FrontendController@payment')->middleware('verified');
-    Route::post('payment/balance', 'PaymentController@balance')->name('payment.balance')->middleware('verified');
-    Route::get('checkout/payment', 'CartController@cek')->name('payment')->middleware('verified');
-    // Route::post('checkout/payment', 'PaymentController@createInvoice')->name('payment')->middleware('verified');
-    Route::get('balance', 'PaymentController@getSaldo')->middleware('verified');
+//     Route::get('payment', 'FrontendController@payment')->middleware('verified');
+//     Route::post('payment/balance', 'PaymentController@balance')->name('payment.balance')->middleware('verified');
+//     Route::get('checkout/payment', 'CartController@cek')->name('payment')->middleware('verified');
+//     // Route::post('checkout/payment', 'PaymentController@createInvoice')->name('payment')->middleware('verified');
+//     Route::get('balance', 'PaymentController@getSaldo')->middleware('verified');
 
-    Route::get('cart', 'CartController@index')->name('cart')->middleware('verified');
-    Route::post('cart/simpan', 'CartController@simpan')->name('cart.simpan')->middleware('verified');
-    Route::get('cart/{id}/delete', 'CartController@delete')->name('cart.delete')->middleware('verified');
-    Route::get('cart/status/{kode_booking}', 'CartController@status')->name('cart.status')->middleware('verified');
-    Route::post('checkout', 'CartController@checkout')->name('checkout')->middleware('verified');
-    Route::get('checkout/{id}', 'CartController@chart')->name('checkout.id')->middleware('verified');
-    Route::post('checkout/{id}/delete', 'CartController@delete')->name('checkout.delete')->middleware('verified');
+//     Route::get('cart', 'CartController@index')->name('cart')->middleware('verified');
+//     Route::post('cart/simpan', 'CartController@simpan')->name('cart.simpan')->middleware('verified');
+//     Route::get('cart/{id}/delete', 'CartController@delete')->name('cart.delete')->middleware('verified');
+//     Route::get('cart/status/{kode_booking}', 'CartController@status')->name('cart.status')->middleware('verified');
+//     Route::post('checkout', 'CartController@checkout')->name('checkout')->middleware('verified');
+//     Route::get('checkout/{id}', 'CartController@chart')->name('checkout.id')->middleware('verified');
+//     Route::post('checkout/{id}/delete', 'CartController@delete')->name('checkout.delete')->middleware('verified');
 
+// });
+
+Route::middleware('web')->domain(env('APP_URL'))->group(function(){
+    Route::get('/', 'FrontendController@index')->name('frontend');
+});
+
+Route::middleware('web')->domain('app.'.env('APP_URL'))->group(function(){
+    Route::get('/', 'Apps\HomeController@index');
 });
 
 // Route::group(['middleware' => 'auth'], function () {
