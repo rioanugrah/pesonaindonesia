5e43ff8e5ac8618155ce3afda993cb4c3391eb7c
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2023-03-05 12:51:31 +0700

    Plesiran Indonesia

--- resources/views/emails/messageInvoice.blade.php
+++ resources/views/emails/messageInvoice.blade.php
@@ -1,2 +1,5 @@
 <div>Terimakasih {{ $details['nama_pembayaran'] }} telah order tiket dikami.</div>
-<div>Kami mengirimkan untuk anda.</div>
+<div>Kami mengirimkan bukti invoice untuk anda.</div>
+<p>Team Marketing</p>
+<img src="{{ public_path('invoice/assets/img/logo_plesiran_new_black2.webp') }}" alt="Logo PPI"
+width="190">
