b22899282b693849be22a60cd65a500c755fdb7d
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2023-03-21 20:02:03 +0700

    Plesiran Indonesia

--- routes/partner.php
+++ routes/partner.php
@@ -6,6 +6,6 @@ use Illuminate\Http\Request;
 Auth::routes(['verify' => true]);
 
 // Route::domain('partner.plesiranindonesia.com')->group(function () {
-Route::domain('partner.'.parse_url(env('APP_URL'), PHP_URL_HOST))->group(function () {
-    Route::get('home', 'FrontendController@partnership')->name('frontend.partnership');
-});
+// Route::domain('partner.'.parse_url(env('APP_URL'), PHP_URL_HOST))->group(function () {
+//     Route::get('home', 'FrontendController@partnership')->name('frontend.partnership');
+// });
