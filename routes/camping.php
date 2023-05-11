247c0d59405409ad75c7c1abe5ce4f1c864510c4
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2023-05-11 22:32:03 +0700

    Plesiran Indonesia

--- routes/camping.php
+++ routes/camping.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 Auth::routes(['verify' => true]);
 
-Route::domain('camping.'.parse_url(env('APP_URL'), PHP_URL_HOST))->group(function () {
+Route::domain('campingadventure.'.parse_url(env('APP_URL'), PHP_URL_HOST))->group(function () {
     // Route::get('/', function(){
     //     return view('layouts.camping.index');
     // });
