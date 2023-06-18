0ed4fb0a2cc4ad1861d92ce9d34324e062775587
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2023-06-18 23:02:26 +0700

    Plesiran Indonesia

--- resources/views/backend_new_2023/dashboard/index.blade.php
+++ resources/views/backend_new_2023/dashboard/index.blade.php
@@ -2,3 +2,74 @@
 @section('title')
     Dashboard
 @endsection
+@section('content')
+    @component('common-components.breadcrumb')
+        @slot('pagetitle')
+            @yield('title')
+        @endslot
+        @slot('title')
+            @yield('title')
+        @endslot
+    @endcomponent
+
+    <div class="row">
+        <div class="col-md-6 col-xl-3">
+            <div class="card">
+                <div class="card-body">
+                    <div>
+                        <h4 class="mb-1 mt-1">Rp. <span data-plugin="counterup">0</span></h4>
+                        <p class="text-muted mb-0">Balance</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Sales Analytics</h4>
+                    <div class="mt-3">
+                        <ul class="list-inline main-chart mb-0">
+                            <li class="list-inline-item chart-border-left me-0 border-0">
+                                <h3 class="text-primary">Rp. <span data-plugin="counterup">0</span><span class="text-muted d-inline-block font-size-15 ms-3">Income</span></h3>
+                            </li>
+                            <li class="list-inline-item chart-border-left me-0">
+                                <h3><span data-plugin="counterup">0</span><span class="text-muted d-inline-block font-size-15 ms-3">Sales</span>
+                                </h3>
+                            </li>
+                            <li class="list-inline-item chart-border-left me-0">
+                                <h3><span data-plugin="counterup">0</span>%<span class="text-muted d-inline-block font-size-15 ms-3">Conversation Ratio</span></h3>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Booking Travelling</h4>
+                    <table id="datatable" class="table table-bordered dt-responsive nowrap"
+                        style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>Kode Booking</th>
+                                <th>Nama Order</th>
+                                <th>Tanggal Booking</th>
+                                <th>Payment Metode</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
