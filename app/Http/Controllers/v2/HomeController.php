c85aca566aa95470f8e5aee2d1ecf3f61182d61c
Rio Anugrah Adam Saputra <rioanugrah999@gmail.com>
2023-06-19 20:48:55 +0700

    Plesiran Indonesia

--- app/Http/Controllers/v2/HomeController.php
+++ app/Http/Controllers/v2/HomeController.php
@@ -5,10 +5,33 @@ namespace App\Http\Controllers\v2;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\v2\TourOrder;
+use \Carbon\Carbon;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('backend_new_2023.dashboard.index');
+        // $data['start_month'] = Carbon::now()->startOfYear();
+        // $data['now_month'] = Carbon::now();
+        $data['periode'] = [];
+        $data['tanggals'] = now()->subMonths(12)->monthsUntil(now());
+        foreach ($data['tanggals'] as $key => $date) {
+            // $data['periode'][] = [
+            //     'month' => $date->shortMonthName,
+            //     'year' => $date->year,
+            // ];
+            $data['total_sales_tour'][] = TourOrder::where(\DB::raw("DATE_FORMAT(created_at, '%Y-m')"),$date->format('Y-m'))->count();
+            $data['periode'][] = $date->format('m/d/Y');
+        }
+        $data['label_total_sales_tour'] = json_encode($data['total_sales_tour']);
+        $data['label_periode'] = json_encode($data['periode']);
+        $data['tour_orders'] = TourOrder::paginate(10);
+        $total_sales_income = TourOrder::all();
+        $data['sales_income'] = $total_sales_income;
+        // dd($data);
+        // dd(json_encode($data['periode']));
+        // dd(json_decode(json_encode($data['periode']),false));
+        return view('backend_new_2023.dashboard.index',$data);
     }
 }
--- resources/views/backend_new_2023/dashboard/index.blade.php
+++ resources/views/backend_new_2023/dashboard/index.blade.php
@@ -33,16 +33,23 @@
                     <div class="mt-3">
                         <ul class="list-inline main-chart mb-0">
                             <li class="list-inline-item chart-border-left me-0 border-0">
-                                <h3 class="text-primary">Rp. <span data-plugin="counterup">0</span><span class="text-muted d-inline-block font-size-15 ms-3">Income</span></h3>
+                                <h3 class="text-primary">Rp. <span data-plugin="counterup">{{ number_format($sales_income->sum('price'),0,',','.') }}</span><span
+                                        class="text-muted d-inline-block font-size-15 ms-3">Income</span></h3>
                             </li>
                             <li class="list-inline-item chart-border-left me-0">
-                                <h3><span data-plugin="counterup">0</span><span class="text-muted d-inline-block font-size-15 ms-3">Sales</span>
+                                <h3><span data-plugin="counterup">{{ $sales_income->count() }}</span><span
+                                        class="text-muted d-inline-block font-size-15 ms-3">Sales</span>
                                 </h3>
                             </li>
                             <li class="list-inline-item chart-border-left me-0">
-                                <h3><span data-plugin="counterup">0</span>%<span class="text-muted d-inline-block font-size-15 ms-3">Conversation Ratio</span></h3>
+                                <h3><span data-plugin="counterup">0</span>%<span
+                                        class="text-muted d-inline-block font-size-15 ms-3">Conversation Ratio</span></h3>
                             </li>
                         </ul>
+                    </div>
+                    <div class="mt-3">
+                        <div id="sales-analytics-chart" data-colors='["--bs-primary", "#dfe2e6", "--bs-warning"]'
+                            class="apex-charts" dir="ltr"></div>
                     </div>
                 </div>
             </div>
@@ -66,10 +73,118 @@
                                 <th>Action</th>
                             </tr>
                         </thead>
-                        <tbody></tbody>
+                        <tbody>
+                            @forelse ($tour_orders as $tour_order)
+                                <tr>
+                                    <td>{{ $tour_order->kode_order }}</td>
+                                    <td>{{ $tour_order->nama_order }}</td>
+                                    <td>{{ $tour_order->tanggal_booking }}</td>
+                                    <td>{{ $tour_order->payment_metode }}</td>
+                                    <td>{{ $tour_order->status }}</td>
+                                    <td></td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6" class="text-center">Data Belum Tersedia</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+@section('script')
+    <script src="{{ URL::asset('backend_new/libs/datatables/datatables.min.js') }}"></script>
+    <script src="{{ URL::asset('backend_new/libs/apexcharts/apexcharts.min.js') }}"></script>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        var options = {
+            chart: {
+                height: 339,
+                type: 'line',
+                stacked: false,
+                toolbar: {
+                    show: false
+                }
+            },
+            stroke: {
+                width: [0, 2, 4],
+                curve: 'smooth'
+            },
+            plotOptions: {
+                bar: {
+                    columnWidth: '30%'
+                }
+            },
+            colors: ['#5b73e8', '#dfe2e6', '#f1b44c'],
+            series: [{
+                    name: 'Desktops',
+                    type: 'column',
+                    data: [{!! $label_total_sales_tour !!}]
+                },
+                // {
+                //     name: 'Laptops',
+                //     type: 'area',
+                //     data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]
+                // }, 
+                // {
+                //     name: 'Tablets',
+                //     type: 'line',
+                //     data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]
+                // }
+            ],
+            fill: {
+                opacity: [0.85, 0.25, 1],
+                gradient: {
+                    inverseColors: false,
+                    shade: 'light',
+                    type: "vertical",
+                    opacityFrom: 0.85,
+                    opacityTo: 0.55,
+                    stops: [0, 100, 100, 100]
+                }
+            },
+            labels: {!! $label_periode !!},
+            // labels: ['01/20/2023'],
+            // labels: ['01/01/2003', '02/01/2003', '03/01/2003', '04/01/2003', '05/01/2003', '06/01/2003', '07/01/2003',
+            //     '08/01/2003', '09/01/2003', '10/01/2003', '11/01/2003'
+            // ],
+            markers: {
+                size: 0
+            },
+            xaxis: {
+                type: 'datetime'
+            },
+            yaxis: {
+                title: {
+                    text: 'Points'
+                }
+            },
+            tooltip: {
+                shared: true,
+                intersect: false,
+                y: {
+                    formatter: function formatter(y) {
+                        if (typeof y !== "undefined") {
+                            return y.toFixed(0) + " points";
+                        }
+
+                        return y;
+                    }
+                }
+            },
+            grid: {
+                borderColor: '#f1f1f1'
+            }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#sales-analytics-chart"), options);
+        chart.render();
+    </script>
 @endsection
